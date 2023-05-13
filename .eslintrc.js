584d675a4164db1159115aca8e0b6d6b1e1a3380
DTrombett <d@trombett.org>
2023-05-13 16:28:20 +0200

    fix: remove dll files missing error

--- .eslintrc.js
+++ .eslintrc.js
@@ -200,9 +200,7 @@ module.exports = {
 		"import/resolver": {
 			// See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
 			node: {},
-			webpack: {
-				config: require.resolve("./.erb/configs/webpack.config.eslint.ts"),
-			},
+			webpack: {},
 			typescript: {},
 		},
 		"import/parsers": {
