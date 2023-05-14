b867880ecc57e359efb73850d2685b2774044b01
DTrombett <maxtromb.dt@gmail.com>
2023-05-14 19:23:52 +0200

    feat: disallow unsafe navigation

--- src/main/utils/resolvePath.ts
+++ src/main/utils/resolvePath.ts
@@ -1,13 +1,13 @@
+import { app } from "electron";
 import { resolve } from "node:path";
 import { env } from "node:process";
 import { URL } from "node:url";
 
 export default (hash: string) => {
-	if (env.NODE_ENV === "development") {
-		const url = new URL(`http://localhost:${env.PORT ?? 1212}/index.html`);
+	if (app.isPackaged)
+		return `file://${resolve(__dirname, "../renderer/index.html#/", hash)}`;
+	const url = new URL(`http://localhost:${env.PORT ?? 1212}/index.html`);
 
-		url.hash = `#/${hash}`;
-		return url.href;
-	}
-	return `file://${resolve(__dirname, "../renderer/index.html")}#/${hash}`;
+	url.hash = `#/${hash}`;
+	return url.href;
 };
